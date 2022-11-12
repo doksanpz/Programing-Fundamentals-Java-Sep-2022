b1fc5555674a76978025b26fc4337320e0cc5ab5
doksanpz <doksanpz@abv.bg>
2022-11-13 01:11:44 +0200

    Add tasks for TextProcessing
    Problem solution for TextProcessing

---
+++ textProcessing/P05digitsLettersAndOther.java
@@ -0,0 +1,31 @@
+package textProcessing;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class P05digitsLettersAndOther {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        StringBuilder digitSb = new StringBuilder();
+        StringBuilder letterSb = new StringBuilder();
+        StringBuilder otherSb = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+
+            if (Character.isDigit(symbol)) {
+                digitSb.append(symbol);
+            } else if (Character.isLetter(symbol)) {
+                letterSb.append(symbol);
+            } else {
+                otherSb.append(symbol);
+            }
+        }
+        System.out.println(digitSb);
+        System.out.println(letterSb);
+        System.out.println(otherSb);
+    }
+}
